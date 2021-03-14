c1f4f2e5a7d0898e5b85810a3e0db9ca2d6818fc
jessilin <jessilinlugo@gmail.com>
2021-03-14 12:55:01 -0700

    adding newly created schema

---
+++ models/Workouts.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: () => new Date()
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                required: "Need Exercise Type",
+            },
+            name: {
+                type: String,
+                required: "Need Exercise Name",
+            },
+            duration: {
+                type: Number,
+                required: "Need Exercise Duration",
+            },
+            distance: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            }
+        }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.export = Workout;
