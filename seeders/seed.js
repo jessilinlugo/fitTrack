a97f4812323bd234af9b14ca1d70dd3136b7e9c4
jessilin <jessilinlugo@gmail.com>
2021-03-21 20:48:11 -0700

    adding env connection to seed

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,10 +1,13 @@
 let mongoose = require("mongoose");
 let db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// mongoose.connect("mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false
+// });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, useCreateIndex: true });
+
 
 let workoutSeed = [
   {
